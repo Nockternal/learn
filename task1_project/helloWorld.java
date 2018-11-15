603f91c9c3d385916764b860591e6988a21b1472
Nockternal <nockternalsa@gmail.com>
2018-11-15 13:33:11 +0200

    Branch 1 change

--- task1_project/helloWorld.java
+++ task1_project/helloWorld.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class helloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Git is Awesome");
+
+        Scanner reader=new Scanner(System.in);
+
+        System.out.println("---------------------");
+
+        System.out.println(reader);
     }
 }
